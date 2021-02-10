a98f2a01875735b101997921ac7c0e54236c79df
Ivan Nikolsky <enty8080@gmail.com>
2021-02-10 22:37:51 +0300

    Update helper.py

--- core/helper.py
+++ core/helper.py
@@ -28,6 +28,8 @@ import os
 import socket
 import url_normalize
 
+import requests
+
 from core.storage import local_storage
 
 class helper:
@@ -45,6 +47,18 @@ class helper:
             local_host = "127.0.0.1"
         return local_host
 
+    def head_url(self, url):
+        url = self.normalize_url(url)
+        return requests.head(url, verify=False).headers
+    
+    def get_url(self, url):
+        url = self.normalize_url(url)
+        return requests.get(url, verify=False)
+    
+    def post_url(self, url, data):
+        url = self.normalize_url(url)
+        return requests.post(url, data, verify=False)
+    
     def strip_scheme(self, url):
         url = url.replace('http://', '', 1)
         url = url.replace('https://', '', 1)

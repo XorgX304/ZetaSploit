c5dad18e3862e126ae94836cacbd2ab4437e3e9c
Ivan Nikolsky <enty8080@gmail.com>
2021-02-11 09:35:58 +0300

    Update web_tools.py

--- utils/web_tools.py
+++ utils/web_tools.py
@@ -1,3 +1,29 @@
+#!/usr/bin/env python3
+
+#
+# MIT License
+#
+# Copyright (c) 2020-2021 EntySec
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
 import requests
 import url_normalize
 
@@ -10,9 +36,11 @@ class web_tools:
         url = self.normalize_url(url)
         return requests.get(url, verify=False)
     
-    def post_url(self, url, data):
+    def post_url(self, url, data, headers=None):
         url = self.normalize_url(url)
-        return requests.post(url, data, verify=False)
+        if headers:
+            return requests.post(url, data=data, headers=headers, verify=False)
+        return requests.post(url, data=data, verify=False)
     
     def strip_scheme(self, url):
         url = url.replace('http://', '', 1)
